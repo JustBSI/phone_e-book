044a238ffd8477ae131a41644a92fb70faa172d3
JustBSI <just.b.sergey@gmail.com>
2023-08-21 00:13:48 +0300

    base funcs upgrade

--- main.py
+++ main.py
@@ -1,42 +1,42 @@
-def add_in_pb(info):
+Record = tuple[str, str, str, str, str, str]
+
+
+def to_record(row: str) -> Record:
+    info = row.split('|')
+    return info[0], info[1], info[2], info[3], info[4], info[5]
+
+
+def to_row(record: Record) -> str:
+    return '|'.join(record)
+
+
+def add(info: Record):
     with open("phone_book.txt", "a") as file:
-        file.write('\n'+'|'.join(info))
+        file.write('\n'+to_row(info))
 
 
-def get_from_pb():
+def get() -> list[Record]:
     with open("phone_book.txt", "r") as file:
-        return file.read().splitlines()
+        return [to_record(s) for s in file.read().splitlines()]
 
 
-def find_in_pb(str):
+def find(phrase: str) -> list[Record]:
     with open("phone_book.txt", "r") as file:
-        result = []
-        for line in file:
-            if str in line:
-                result.append(line.rstrip())
-        return result
+        return [to_record(s) for s in file.read().splitlines() if phrase in s]
 
 
-def delete_from_pb(index):
-    with open("phone_book.txt", "r") as temp:
-        pb = temp.readlines()
-    pb.pop(index-1)
+def replace(index: int, info: Record):
+    with open("phone_book.txt", "r") as file:
+        pb = file.readlines()
+    pb[index-1] = to_row(info) + '\n'
     with open("phone_book.txt", "w") as file:
         file.writelines(pb)
 
 
-def replace_in_pb(index, str):
-    with open("phone_book.txt", "r") as temp:
-        pb = temp.readlines()
-    pb[index-1] = str + '\n'
-    with open("phone_book.txt", "w") as file:
-        file.writelines(pb)
-
-
-
-
-# add_in_pb(['Викторов', 'Виктор', 'Викторович', 'Виктория', '99-00-33', '+7-342-574-12-44'])
-# print(get_from_pb())
-# print(find_in_pb('99'))
-# delete_from_pb(3)
-# replace_in_pb(3, 'Дмитрий|Дмитриевич|Дмитров|Фейсбук|32-24-76|+7-902-757-86-53')
+if __name__ == '__main__':
+    # add(('Викторов', 'Виктор', 'Викторович', 'Виктория', '99-00-33', '+7-342-574-12-44'))
+    # print(get())
+    # print(find('99'))
+    # delete(3)
+    # replace(3, ('Дмитрий', 'Дмитриевич', 'Дмитров', 'Фейсбук', '32-24-76', '+7-902-757-86-53'))
+    ...
